d75fd1924fe5c5372ae012ae7a54f9b859dfd872
Wu Jiawen <602956936@qq.com>
2016-06-30 20:36:16 +0800

    连接了服务端

--- Respire/app/src/main/java/com/example/respireapp/Activity/HomeActivity.java
+++ Respire/app/src/main/java/com/example/respireapp/Activity/HomeActivity.java
@@ -1,13 +1,15 @@
-package com.example.respireapp;
+package com.example.respireapp.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.example.respireapp.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_home);
     }
 }
