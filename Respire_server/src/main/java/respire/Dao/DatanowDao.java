5efbeb3c36dee3adc2851c3c51ea8253436d9acd
sherlockkenan <sherlockkenan@outlook.com>
2016-07-01 08:56:21 +0800

    debug

--- Respire_server/src/main/java/respire/Dao/DatanowDao.java
+++ Respire_server/src/main/java/respire/Dao/DatanowDao.java
@@ -14,9 +14,12 @@ import respire.Entity.DatanowPK;
 @Repository 
 public interface DatanowDao extends CrudRepository<Datanow, DatanowPK> {
 		
-	@Query(value="select date_format(time, '%Y-%m-%d-%H')as time,Avg(pm25) as pm25, Avg(so2) as so2,Avg(co2) as co2 from datanow where date_format(now(),'%Y-%m-%d')=DATE_FORMAT(time,'%Y-%m-%d') and userid=:userid group by date_format(time, '%Y-%m-%d-%H') ORDER BY time desc")
-	 public List<Object> getbyday(@Param("userid") long userid);
+	//@Query(value="select date_format(time, '%Y-%m-%d-%H')as time,Avg(pm25) as pm25, Avg(so2) as so2,Avg(co2) as co2 from datanow where date_format(now(),'%Y-%m-%d')=DATE_FORMAT(time,'%Y-%m-%d') and userid=?1 group by date_format(time, '%Y-%m-%d-%H') ORDER BY time desc")
+	@Query("select date_format(time, '%Y-%m-%d-%H')as time,Avg(pm25) as pm25, Avg(so2) as so2,Avg(co2) as co2 from Datanow d where date_format(now(),'%Y-%m-%d')=DATE_FORMAT(time,'%Y-%m-%d') and d.userid=?1 group by date_format(time, '%Y-%m-%d-%H') ORDER BY time desc") 
+	 
+	public List<Object> getbyday( long userid);
 	
-	@Query(value="select date_format(time, '%Y-%m-%d')as time,Avg(pm25) as pm25, Avg(so2) as so2,Avg(co2) as co2 from datanow where date_sub(curdate(), INTERVAL 7 DAY) <= date(time)and userid=:userid group by date_format(time, '%Y-%m-%d') ORDER BY time desc")
-	 public List<Object> getbyweek(@Param("userid") long userid);
+	//@Query("select d from Datanow d where d.userid=?1") 
+	@Query("select date_format(time, '%Y-%m-%d-%H')as time,Avg(pm25) as pm25, Avg(so2) as so2,Avg(co2) as co2 from Datanow d where date_format(now(),'%Y-%m-%d')-DATE_FORMAT(time,'%Y-%m-%d')<=7 and d.userid=?1 group by date_format(time, '%Y-%m-%d-%H') ORDER BY time desc")
+	 public List<Object> getbyweek(long userid);
 }
