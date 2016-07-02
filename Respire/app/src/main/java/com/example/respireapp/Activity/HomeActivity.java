02b7afefeecf940cab0a09fc5c8087e75e18410d
Wu Jiawen <602956936@qq.com>
2016-07-02 15:19:10 +0800

    更多页面

--- Respire/app/src/main/java/com/example/respireapp/Activity/HomeActivity.java
+++ Respire/app/src/main/java/com/example/respireapp/Activity/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.respireapp.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -19,60 +22,45 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HomeActivity extends AppCompatActivity {
-    private static final String INSTAGRAM_CLIENT_ID = "<< YOUR INSTAGRAM CLIENT-ID >>";
-    private static final Object TAG = new Object();
-    private static final String LOG = "VOLLEY-SAMPLE";
-    private RequestQueue mQueue;
-
+public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Bundle bundle=this.getIntent().getExtras();
-       final String JSESSIONID=bundle.getString("sessionid");
-        //final String JSESSIONID="123456";
+        final Button historyButton=(Button) findViewById(R.id.historyButton);
+        final Button discoveryButton=(Button) findViewById(R.id.discoveryButton);
+        final Button meButton=(Button) findViewById(R.id.meButton);
 
-        String url="http://192.168.16.130:8000/login";
-        HashMap<String,String> map=new HashMap<String,String>();
-        String username="kek";
-        map.put("username",username);
-        map.put("password",username);
-
-        JSONObject object=new JSONObject(map);
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url,
-                object, new Response.Listener<JSONObject>(){
-            public void onResponse(JSONObject result){
-                try {
-                    String flag = result.getString("return_type");
-
-//                    Intent tintent=new Intent();
-//                    tintent.putExtra("flag", flag);
-//                    tintent.setAction("com.example.respireapp.Activity.LoginActivity");
-//                    sendBroadcast(tintent);
-                }
-                catch (JSONException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        })
-        {
+        historyButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                super.getHeaders();
-                Map<String,String> headers=new HashMap<String,String>();
-                headers.put("Cookie","JSESSIONID="+JSESSIONID);
-                return headers;
+            public void onClick(View v){
+                Intent intent=new Intent();
+                intent.setClass(HomeActivity.this,HistoryActivity.class);
+                startActivity(intent);
             }
-        };
-        request.setTag(TAG);
-        requestQueue.add(request);
+
+        });
+
+        discoveryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent();
+                intent.setClass(HomeActivity.this,DiscoveryActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        meButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent();
+                intent.setClass(HomeActivity.this,MeActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
     }
 }
