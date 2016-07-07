62fcb73fdd99c6699e2cefcb78516c2ede29b51f
sherlockkenan <sherlockkenan@outlook.com>
2016-07-08 00:34:15 +0800

    地图完善

--- Respire/app/src/main/java/com/example/respireapp/Activity/map/HeatMap.java
+++ Respire/app/src/main/java/com/example/respireapp/Activity/map/HeatMap.java
@@ -37,7 +37,7 @@ import java.util.List;
 public class HeatMap extends Activity {
     private LocationClient mLocClient;
     private BaiduMap mBaiduMap;
-    private Button button;
+    private Button loc;
     MapView mMapView;
     //当前位置经纬度
     private double latitude;
@@ -45,10 +45,15 @@ public class HeatMap extends Activity {
     //是否首次定位
     private boolean isFirstLoc = true;
     private com.baidu.mapapi.map.HeatMap heatmap;
-    private Button mAdd;
+    private Button pm25;
+    private Button co2;
+    private Button so2;
+
 
     //获得的数据
-    List<WeightedLatLng> pm25 = new ArrayList<WeightedLatLng>();
+    List<WeightedLatLng> pm25_list = new ArrayList<WeightedLatLng>();
+    List<WeightedLatLng> so2_list = new ArrayList<WeightedLatLng>();
+    List<WeightedLatLng> co2_list = new ArrayList<WeightedLatLng>();
 
 
 
@@ -64,6 +69,9 @@ public class HeatMap extends Activity {
         // 获取LocationClient
         mLocClient = new LocationClient(this);
 
+
+
+
         LocationClientOption option = new LocationClientOption();
         option.setCoorType("bd09ll");
         mLocClient.setLocOption(option);
@@ -72,19 +80,50 @@ public class HeatMap extends Activity {
         mMapView = (MapView) findViewById(R.id.mapView);
         mBaiduMap = mMapView.getMap();
 
-        button=(Button) findViewById(R.id.button);
-        mAdd = (Button) findViewById(R.id.add);
-        button.setOnClickListener(new View.OnClickListener() {
+        loc=(Button) findViewById(R.id.button);
+        pm25 = (Button) findViewById(R.id.pm25);
+        co2 = (Button) findViewById(R.id.co2);
+        so2 = (Button) findViewById(R.id.so2);
+        pm25.setEnabled(false);
+        so2.setEnabled(false);
+        co2.setEnabled(false);
+        loc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                   ToMyLocation();
             }
         });
-        mAdd.setOnClickListener(new View.OnClickListener() {
+        pm25.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                addHeatMap();
+                heatmap.removeHeatMap();
+                addHeatMap("pm25");
+                pm25.setEnabled(false);
+                so2.setEnabled(true);
+                co2.setEnabled(true);
             }
         });
+        so2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                heatmap.removeHeatMap();
+                addHeatMap("so2");
+                pm25.setEnabled(true);
+                so2.setEnabled(false);
+                co2.setEnabled(true);
+            }
+        });
+        co2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                heatmap.removeHeatMap();
+
+                addHeatMap("co2");
+                co2.setEnabled(false);
+                so2.setEnabled(true);
+                pm25.setEnabled(true);
+            }
+        });
+
+        //获取数据
+        Send_request();
 
         // 显示出当前位置的小图标
         mBaiduMap.setMyLocationEnabled(true);
@@ -92,23 +131,29 @@ public class HeatMap extends Activity {
         MyLocationListener mListener = new MyLocationListener();
         mLocClient.registerLocationListener(mListener);
         mLocClient.start();
-        addHeatMap();
+
     }
 
-    private void addHeatMap() {
+    private void addHeatMap(String type) {
         final Handler h = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
-                //mBaiduMap.addHeatMap(heatmap);
-                mAdd.setEnabled(false);
+                mBaiduMap.addHeatMap(heatmap);
+
             }
         };
+        final String name=type;
         new Thread() {
             @Override
             public void run() {
                 super.run();
-                Send_pm25_request();
+                if(name=="pm25")
+                    heatmap = new com.baidu.mapapi.map.HeatMap.Builder().weightedData(pm25_list).build();
+                if(name=="so2")
+                    heatmap = new com.baidu.mapapi.map.HeatMap.Builder().weightedData(so2_list).build();
+                if(name=="co2")
+                    heatmap = new com.baidu.mapapi.map.HeatMap.Builder().weightedData(co2_list).build();
                 h.sendEmptyMessage(0);
             }
         }.start();
@@ -118,7 +163,8 @@ public class HeatMap extends Activity {
 
 
     //发送请求
-    public void  Send_pm25_request(){
+    public void  Send_request(){
+
         String url="http://192.168.1.119:8000/map/getalldata";
         RequestQueue requestQueue= Volley.newRequestQueue(this);
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,
@@ -130,13 +176,20 @@ public class HeatMap extends Activity {
                         JSONObject temp= (JSONObject)returnvalue.get(i);
                         double lat=temp.getDouble("latitude");
                         double lon=temp.getDouble("longitude");
-                        double value =temp.getDouble("pm25");
-                        pm25.add(new WeightedLatLng(new LatLng(lat,lon),value));
+                        double value_pm25 =temp.getDouble("pm25");
+                        double value_so2 =temp.getDouble("so2");
+                        double value_co2 =temp.getDouble("co2");
+                        pm25_list.add(new WeightedLatLng(new LatLng(lat,lon),value_pm25));
+                        co2_list.add(new WeightedLatLng(new LatLng(lat,lon),value_co2));
+                        so2_list.add(new WeightedLatLng(new LatLng(lat,lon),value_so2));
 
                     }
 
-                    heatmap = new com.baidu.mapapi.map.HeatMap.Builder().weightedData(pm25).build();
-                    mBaiduMap.addHeatMap(heatmap);
+                    pm25.setEnabled(true);
+                    so2.setEnabled(true);
+                    co2.setEnabled(true);
+
+                    addHeatMap("pm25");
 
                 }
                 catch (JSONException e) {
