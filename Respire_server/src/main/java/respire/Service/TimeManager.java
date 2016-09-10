995c2bb72abec7aecbec4094329cdf32d15c6d65
sherlockkenan <sherlockkenan@outlook.com>
2016-09-10 13:42:48 +0800

    fix server

--- Respire_server/src/main/java/respire/Service/TimeManager.java
+++ Respire_server/src/main/java/respire/Service/TimeManager.java
@@ -20,7 +20,7 @@ public class TimeManager {
 
 
 	//时间间隔(一天)
-	private static final long PERIOD_DAY = 24 * 60 * 60 * 1000;
+	private static final long PERIOD_DAY =  1000;
 	public TimeManager() {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.HOUR_OF_DAY, 1); //凌晨1点
